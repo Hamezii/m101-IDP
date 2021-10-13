fd652ccd006763995dc0c70bba7602d467b1eadc
Derek710 <55542812+Derek710@users.noreply.github.com>
2021-10-13 17:04:00 +0100

    1

--- Elec/IR distance/IR_distance_10-80/IR_distance_10-80.ino
+++ Elec/IR distance/IR_distance_10-80/IR_distance_10-80.ino
@@ -9,9 +9,18 @@ void setup() {
 void loop() {
   float Vir = analogRead(receivePin);
   // get voltage
-  delay(60); // test period <=50ms
-  
+  delay(100); // test period <=50ms
+
+  if((Vir<600)&&(Vir>60)){
   //Serial.println(Vir);
   float distance = 26.39*pow((Vir*5/1024),(-1.245)); // in cm
   Serial.println(distance);
+  }
+  else{
+    Serial.println(0);
+
+    
+  }
+  
+  
 }
